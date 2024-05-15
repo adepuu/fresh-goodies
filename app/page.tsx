f4ced2362e5e8105d1dbece3de0f3143e23e2bd3
Yosef1997 <yosefsitumorang97@gmail.com>
2024-05-15 13:35:13 +0700

    feat(hooks): fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
+import useFetchCart from "@/hooks/useFetchCart"
 import useFetchFavProd from "@/hooks/useFetchFavProd"
 import Image from "next/image"
 
 export default function Home() {
   const { favProd } = useFetchFavProd()
+  const { cartList } = useFetchCart()
 
   console.log(favProd)
   return <main className='min-h-screen'></main>
